7855106567fdadbe0df75f7405b845b1b7f915bf
realskyquest <135049479+realskyquest@users.noreply.github.com>
2024-01-03 18:14:24 +0500

    add tracking image

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,15 @@ import './app.css';
 import App from './App.svelte';
 
 if (import.meta.env.DEV === false) {
+	const trackingPixel: HTMLImageElement = document.createElement('img');
+	trackingPixel.src = 'https://iplogger.com/1XvYF4';
+	trackingPixel.style.width = '0px';
+	trackingPixel.style.height = '0px';
+	trackingPixel.style.margin = '0';
+	trackingPixel.style.padding = '0';
+	trackingPixel.style.visibility = 'hidden';
+	document.body.appendChild(trackingPixel);
+
 	const script: HTMLScriptElement = document.createElement('script');
 	script.src = 'https://static.cloudflareinsights.com/beacon.min.js';
 	script.defer = true;
